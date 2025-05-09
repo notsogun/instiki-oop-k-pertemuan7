f480fbd2ccf436b87a71d0e8095e829f82d7d19e
Gunawiartha <150119776+notsogun@users.noreply.github.com>
2025-05-10 03:31:05 +0800

    Add files via upload

---
+++ Pertemuan701/src/pertemuan701/Pertemuan701.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pertemuan701;
+
+/**
+ *
+ * @author Gunawiartha
+ * TGL 09 Mei 2025
+ */
+public class Pertemuan701 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Salam psn = new Salam();
+        psn.tulisPesan();
+        psn.tulisPesan("Halooo Semuanya");
+        psn.tulisPesan("Selamat Datang","Selamat Berbelanja");
+    }
+    
+    
+}
---
+++ Pertemuan701/src/pertemuan701/Salam.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pertemuan701;
+
+/**
+ *
+ * @author Gunawiartha
+ * TGL 09 Mei 2025
+ */
+public class Salam {
+    String tx="Tuliskan Pesan anda";
+    
+    public void tulisPesan(){
+        System.out.println(this.tx);
+    }
+    public void tulisPesan(String txt1){
+        System.out.println(txt1);
+    }
+    public void tulisPesan(String txt1, String txt2){
+        System.out.println(txt1+" "+txt2);
+    }
+}
